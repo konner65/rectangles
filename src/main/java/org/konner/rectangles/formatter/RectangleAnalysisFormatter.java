8d6626e0bed33b3bea4ec6ed9e66a430694d0066
Konner Kite <konner65@gmail.com>
2026-04-17 17:02:52 -0500

    Add todos

--- src/main/java/org/konner/rectangles/formatter/RectangleAnalysisFormatter.java
+++ src/main/java/org/konner/rectangles/formatter/RectangleAnalysisFormatter.java
@@ -1,5 +1,8 @@
 package org.konner.rectangles.formatter;
 
+import org.konner.rectangles.model.RectangleAnalysisResult;
+
+//todo implement interface as spring boot component
 public interface RectangleAnalysisFormatter {
-    String format(Ana)
+    String format(RectangleAnalysisResult result);
 }
