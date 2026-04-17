895c0a56fb6d45a0cb610e10f17de9e82e0cf508
Konner Kite <konner65@gmail.com>
2026-04-17 17:02:52 -0500

    Add todos

--- src/main/java/org/konner/rectangles/service/RectangleAnalysisService.java
+++ src/main/java/org/konner/rectangles/service/RectangleAnalysisService.java
@@ -2,6 +2,7 @@ package org.konner.rectangles.service;
 
 import org.konner.rectangles.model.*;
 
+//todo implement interface as spring boot component
 public interface RectangleAnalysisService {
     Intersection intersection(Rectangle r1, Rectangle r2);
     Containment containment(Rectangle r1, Rectangle r2);
